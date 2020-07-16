3a58bc90460cb4cecee8258fd4753ad56d1b5cc7
Allan <allanhsz@outlook.com>
2020-07-16 17:15:23 -0300

    Adicionando DELETE categoria

--- src/main/java/com/allanhsz/cursomc/services/CategoriaService.java
+++ src/main/java/com/allanhsz/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.allanhsz.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.allanhsz.cursomc.domain.Categoria;
 import com.allanhsz.cursomc.repositories.CategoriaRepository;
+import com.allanhsz.cursomc.services.exceptions.DataIntegrityException;
 import com.allanhsz.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -28,6 +30,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos.");
+		}
 	}
 	
 }
