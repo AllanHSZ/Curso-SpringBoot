8463e1439ebf86c296d2fc8c6fc4f7006e1e4773
Allan <allanhsz@outlook.com>
2020-07-16 18:26:17 -0300

    Categoria adicionando paginação

--- src/main/java/com/allanhsz/cursomc/services/CategoriaService.java
+++ src/main/java/com/allanhsz/cursomc/services/CategoriaService.java
@@ -1,9 +1,13 @@
 package com.allanhsz.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.allanhsz.cursomc.domain.Categoria;
@@ -20,6 +24,10 @@ public class CategoriaService {
 	public Categoria find(Integer id) {  
 		Optional<Categoria> obj = repo.findById(id);  
 		return obj.orElseThrow(() -> new ObjectNotFoundException( "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public List<Categoria> findAll() {  
+		return repo.findAll();
 	}
 	
 	public Categoria insert(Categoria obj) {
@@ -39,6 +47,11 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos.");
 		}
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 	
 }
