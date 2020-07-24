f4aa0c1add61e8937ded6a6db552626d7e55d59a
Allan <allanhsz@outlook.com>
2020-07-24 17:38:14 -0300

    Restricao de conteudo: cliente so recupera seus pedidos

--- src/main/java/com/allanhsz/cursomc/services/PedidoService.java
+++ src/main/java/com/allanhsz/cursomc/services/PedidoService.java
@@ -4,16 +4,24 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.allanhsz.cursomc.domain.Cliente;
 import com.allanhsz.cursomc.domain.ItemPedido;
 import com.allanhsz.cursomc.domain.PagamentoComBoleto;
 import com.allanhsz.cursomc.domain.Pedido;
 import com.allanhsz.cursomc.domain.enums.EstadoPagamento;
+import com.allanhsz.cursomc.repositories.ClienteRepository;
 import com.allanhsz.cursomc.repositories.ItemPedidoRepository;
 import com.allanhsz.cursomc.repositories.PagamentoRepository;
 import com.allanhsz.cursomc.repositories.PedidoRepository;
+import com.allanhsz.cursomc.resources.UserService;
+import com.allanhsz.cursomc.security.UserSS;
+import com.allanhsz.cursomc.services.exceptions.AuthorizationException;
 import com.allanhsz.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -38,6 +46,18 @@ public class PedidoService {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		UserSS user = UserService.authenticated();
+		if(user == null)
+			throw new AuthorizationException("Acesso negado");
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		
+		return repo.findByCliente(cliente, pageRequest);
 	}
 
 	@Transactional
